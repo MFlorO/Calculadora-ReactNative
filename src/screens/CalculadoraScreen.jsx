39182a77ee364e7de9a23f571468fbaa20768293
Flori O <floroldani@hotmail.com>
2023-05-17 17:18:36 -0300

    limpiar el componente screen de la logica

--- src/screens/CalculadoraScreen.jsx
+++ src/screens/CalculadoraScreen.jsx
@@ -1,46 +1,51 @@
 import { Text, View } from 'react-native';
 import { styles } from '../theme/calculadoraTheme';
 import BotonCalc from '../components/BotonCalc';
+import useCalculadora from '../hook/useCalculadora';
+
 
 
 const CalculadoraScreen = () => {
+
+  const { numeroAnterior, numero, limpiar, armarNumero, PositivoNegativo, del, btnSumar, btnRestar, btnMultiplicar, btnDividir, calcular } = useCalculadora()
+
   return (
     <View style={styles.calculadoraContainer}>
-      <Text style={styles.resultadoPequeño}>1,000.50</Text>
-      <Text style={styles.resultado}>1,000.50</Text>
+      { numeroAnterior !== '0' && <Text style={styles.resultadoPequeño}>{numeroAnterior}</Text>}
+      <Text style={styles.resultado} numberOfLines={1} adjustsFontSizeToFit>{numero}</Text>
 
       <View style={styles.fila}>
-        <BotonCalc text="C" color='#9B9B9B' textColor='black'/>
-        <BotonCalc text="+/-" color='#9B9B9B' textColor='black'/>
-        <BotonCalc text="%" color='#9B9B9B' textColor='black'/>
-        <BotonCalc text="/" color='#FF9427' textColor='white'/>
+        <BotonCalc text="C" color='#9B9B9B' textColor='black' onPress={limpiar}/>
+        <BotonCalc text="+/-" color='#9B9B9B' textColor='black' onPress={PositivoNegativo}/>
+        <BotonCalc text="del" color='#9B9B9B' textColor='black' onPress={del}/>
+        <BotonCalc text="/" color='#e66dc6' textColor='white' onPress={btnDividir}/>
       </View>
 
       <View style={styles.fila}>
-        <BotonCalc text="7" color='#424141' />
-        <BotonCalc text="8" color='#424141' />
-        <BotonCalc text="9" color='#424141' />
-        <BotonCalc text="x" color='#FF9427' />
+        <BotonCalc text="7" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="8" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="9" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="x" color='#e66dc6' onPress={btnMultiplicar}/>
       </View>
 
       <View style={styles.fila}>
-        <BotonCalc text="4" color='#424141' />
-        <BotonCalc text="5" color='#424141' />
-        <BotonCalc text="6" color='#424141' />
-        <BotonCalc text="-" color='#FF9427' />
+        <BotonCalc text="4" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="5" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="6" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="-" color='#e66dc6' onPress={btnRestar}/>
       </View>
 
       <View style={styles.fila}>
-        <BotonCalc text="1" color='#424141' />
-        <BotonCalc text="2" color='#424141' />
-        <BotonCalc text="3" color='#424141' />
-        <BotonCalc text="+" color='#FF9427' />
+        <BotonCalc text="1" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="2" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="3" color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="+" color='#e66dc6' onPress={btnSumar}/>
       </View>
 
       <View style={styles.fila}>
-        <BotonCalc text="0" color='#424141' ancho/>
-        <BotonCalc text="." color='#424141' />
-        <BotonCalc text="=" color='#FF9427' />
+        <BotonCalc text="0" color='#424141' onPress={armarNumero} ancho/>
+        <BotonCalc text="." color='#424141' onPress={armarNumero}/>
+        <BotonCalc text="=" color='#e66dc6' onPress={calcular}/>
       </View>
 
     </View>
